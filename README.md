8ff26d48d101a5f33dbcd34a84b096e1905b5d34
Allan Nava <allannava95@gmail.com>
2020-12-30 20:50:50 +0100

    testing

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Task-List
 
 Simple task list in Go Lang
+
+It use:
+
+1. https://gorm.io/docs/ ( go orm )
+2. https://github.com/gorilla/mux ( router )
+
+
+https://github.com/oauth2-proxy/oauth2-proxy
--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import(
 	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
 )
 
 func main()  {
-	fmt.Print("test main.go")
+	fmt.Print("start main task list")
 	
+	r := mux.NewRouter()
+    //r.HandleFunc("/", HomeHandler)
+    //r.HandleFunc("/products", ProductsHandler)
+    //r.HandleFunc("/articles", ArticlesHandler)
+	http.Handle("/", r)
+	//
 }
